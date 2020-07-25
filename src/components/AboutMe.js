36be9f5edd48dbc4bba94a420921a52283a77372
Nattapon Srivithayarat <aong@Nattapons-MacBook-Pro.local>
2020-07-25 20:41:54 +0700

    moving text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import $ from 'jquery';
 
 class AboutMe extends Component {
 
@@ -6,9 +7,13 @@ constructor(props) {
   super(props)
 }
 
-  componentDidMount() {
-   
-  }
+componentDidMount() {
+  $(document).on('scroll', () => {
+    
+    // $('.about .rotate').css('transform',  'rotate(' + Math.max(0 + 1.3 * window.scrollY) + 'deg)')
+    // console.log(window.scrollY)
+  })
+}
   
   render() {
     console.log('AbouMe Rendering')
@@ -17,7 +22,7 @@ constructor(props) {
       const { age, fname, lname } = this.props.Personal
       
       return(
-        <div className="bg-gray-900  min-h-half">
+        <div className="bg-gray-900  min-h-half about">
           {/* <p className="text-white text-7r">{ sign }</p> */}
           <div className="
               flex bg-gray-900  min-h-screen  xl:min-h-half items-center  justify-center mx-auto max-w-screen-xs
@@ -26,13 +31,13 @@ constructor(props) {
             ">
             <div className="wrap flex flex-row items-center ">
               <div className="flex text-center justify-center w-2.5r md:w-12r ">
-                <p className="text-white text-base md:text-3r lg:text-6.5r">{'<'}</p>
+                <p className="text-white text-base md:text-3r lg:text-6.5r rotate">{'<'}</p>
               </div>
               <div className="flex w-15r md:w-25r justify-center">
-                <p className="text-white text-sm md:text-base lg:text-lg ">Hello ! I'm {fname} {lname} {age} year-old, I am a Programmer working on creating a website sometime I make frontend sometime I make backend and sometime I making full of the website</p>      
+                <p className="text-white text-sm md:text-base lg:text-lg "><span className='text-2xl'>H</span>ello ! I'm {fname} {lname} {age} year-old, I am a Programmer working on creating a website sometime I make frontend sometime I make backend and sometime I making full of the website</p>      
               </div>
               <div className="flex text-center justify-center w-2.5r md:w-12r">
-                <p className="text-white text-base md:text-3r lg:text-6.5r">{'/>'}</p>
+                <p className="text-white text-base md:text-3r lg:text-6.5r rotate">{'/>'}</p>
               </div>
             </div>
           </div>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import $ from 'jquery';
 
 
 class Intro extends Component {
   componentDidMount() {
-    // console.log('Mount => Intro')
+    $(document).on('scroll', () => {
+      $('.intro .scrollingLeft').css('left', Math.max(0 - 1.3 * window.scrollY) + 'px')
+      $('.intro .scrollingRight').css('left', Math.max(0 + 1.3 * window.scrollY) + 'px')
+      // console.log(window.scrollY)
+    })
   }
   render() {
     return(
       // <div className="bg-black flex justify-center items-center min-h-screen text-white">
-      <div className="bg-gray-900 min-h-screen">
+      <div className="bg-gray-900 min-h-screen intro">
         <div className="
             flex bg-gray-900 min-h-screen items-center justify-center mx-auto max-w-screen-xs
             sm:max-w-screen-sm
@@ -16,8 +21,8 @@ class Intro extends Component {
           ">
           <div className="flex flex-col-reverse md:flex-row lg:flex-row items-center">
             <div className="w-1/2 z-10 md:mr-ne8 lg:mr-ne14">
-              <h1 className="text-4r text-center md:text-right md:text-7r lg:text-9r text-white">Hello</h1>
-              <h2 className="text-1.35r text-center md:text-right md:text-2r lg:text-3.25r text-white">Im Web Developer</h2>
+              <h1 className="text-4r text-center md:text-right md:text-7r lg:text-9r text-white scrollingLeft">Hello</h1>
+              <h2 className="text-1.35r text-center md:text-right md:text-2r lg:text-3.25r text-white scrollingRight">Im Web Developer</h2>
             </div>
             <div className="w-1/2 py-5">
               <img src="./img/gentleman.jpg" />
@@ -25,7 +30,6 @@ class Intro extends Component {
           </div>         
         </div>
       </div>
-
     )
   }
 }
