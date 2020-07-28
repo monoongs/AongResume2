db7da02fcf17d293d930556f35a2eaa8f97713dc
Nattapon Srivithayarat <monoons@gmail.com>
2020-07-28 19:20:51 +0700

    updated

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class DownloadCV extends Component {
     render() {
         return(
-            <a href="https://drive.google.com/file/d/1K7wlm4ws0BORHzVe7lPQ8pmptTkUUjct/view?usp=sharing" target="_blank">
+            <a href="https://drive.google.com/u/0/uc?id=1K7wlm4ws0BORHzVe7lPQ8pmptTkUUjct&export=download" target="_blank">
                 <div class='download-btn'>
                     <div class="wrap my-auto">
                         <img src="/img/download-btn2.png" width="50px" class="mx-auto" />
