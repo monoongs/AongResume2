e297c3d86052d7258097c33ef06af75701afa680
monoongs <monoongs@gmail.com>
2020-09-05 01:11:31 +0700

    edit link cv

---
+++ public/precache-manifest.e1963b3931928d8825a80c175fa86237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35275ad3dafb718e4d63cd358749f4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21bb8913c8be12b35039",
+    "url": "/static/css/main.a21cc5b3.chunk.css"
+  },
+  {
+    "revision": "9a287f3299a9eea05038",
+    "url": "/static/js/2.7b24b138.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.7b24b138.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21bb8913c8be12b35039",
+    "url": "/static/js/main.cc00a5a6.chunk.js"
+  },
+  {
+    "revision": "b3972c77fbd68e557c91",
+    "url": "/static/js/runtime-main.c902395a.js"
+  }
+]);
--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class DownloadCV extends Component {
     render() {
         return(
-            <a href="https://drive.google.com/u/0/uc?id=1K7wlm4ws0BORHzVe7lPQ8pmptTkUUjct&export=download">
+            <a href="https://drive.google.com/u/0/uc?id=1VzOLoqLGtED2IsfJlL9T-uIb0Wm5l5Sv&export=download">
                 <div class='download-btn'>
                     <div class="wrap my-auto">
                         <img src="/img/download-btn2.png" width="50px" class="mx-auto" />
